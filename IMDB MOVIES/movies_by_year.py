8a051aea2966bd0fc8319722c429dd75f707f41d
Shinjini Shome <122408078+ShinjiniShome@users.noreply.github.com>
2023-10-07 21:49:50 +0200

    Created plot for movies by year

--- IMDB MOVIES/movies_by_year.py
+++ IMDB MOVIES/movies_by_year.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 imdb_movies = pd.read_csv(r"imdb_top_1000.csv")
 print(imdb_movies)
@@ -17,6 +18,21 @@ if int(year) >= 1920 and int(year) <= 2020 :
 else :
     print("Invalid year or year not found in data...")
 
-# Find how many Movies were Released in each Year
-# Show Statistics as a Plot
+# Find how many Movies were Released in each Year and get the top 10 years when highest number of movies were released
+movie_counts_by_year = imdb_movies.groupby('Released_Year').size().reset_index(name='Movie_Count')
+sort_highest_count_years = movie_counts_by_year.sort_values(by="Movie_Count", ascending=False)
+filter_top10_years = sort_highest_count_years.head(10)
+filter_top10_years = filter_top10_years.sort_values(by="Released_Year", ascending=True)
+print(movie_counts_by_year)
+print(sort_highest_count_years)
+print(filter_top10_years)
 
+# Show Statistics of top 10 years as a Plot
+plt.figure(figsize=(12, 8))  # Set the figure size
+plt.bar(filter_top10_years['Released_Year'], filter_top10_years['Movie_Count'])
+plt.xlabel('Year')
+plt.ylabel('Number of Movies Released')
+plt.title('Number of Movies Released In 10 Highest Release Years')
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.show()
+plt.savefig("Plots/Movies_in_Highest_ReleaseYears.png")
